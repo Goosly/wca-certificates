97c6f8a480a3bd7485d37720649bf99642248fcd
Manu Vereecken <manu.vereecken@hotmail.com>
2019-11-29 20:33:56 +0100

    font awesome icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, AngularFontAwesomeModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
